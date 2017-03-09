4e2ffae4fd75d1bd52556b8eb39a62e15ca20e83
parallelipopat <utsavp123@gmail.com>
2017-03-09 09:40:23 +0000

    Update getStockName.py
    Added comments

--- getStockName.py
+++ getStockName.py
@@ -9,10 +9,14 @@ def getStockNames(input_str):
 	search_str = input_str.replace(' ','%20')
 	response = http.request('GET','http://dev.markitondemand.com/Api/v2/Lookup/json?input=' + search_str)
 	if (response.status == 400) :
+		#unlikely to go into this branch (only if API changes)
 		outputResponse = json.dumps("Error")
 	else:
+		#convert the response from bytes to text
 		strResponse = response.data.decode("utf-8")
+		#convert the string into a Python list
 		listResponse = json.loads(strResponse)
+		#filter the list based on the exchanges, and sort the output using the exchange as key
 		fltrlistResponse = sorted([elem for elem in listResponse if elem['Exchange'] in ['NYSE', 'NASDAQ']],key = lambda f : f['Exchange'])
 		outputResponse = json.dumps(fltrlistResponse)
 	return outputResponse
