334a03b304beb3414a8d0f689acc0f113d617f9e
parallelipopat <utsavp123@gmail.com>
2017-03-09 09:36:18 +0000

    Added function to get stock names
    Uses (possibly incomplete) input string to find similar stock names for autocomplete stock name feature.

---
+++ getStockName.py
@@ -0,0 +1,20 @@
+import urllib3
+import json
+import sys
+
+
+http = urllib3.PoolManager()
+
+def getStockNames(input_str):
+	search_str = input_str.replace(' ','%20')
+	response = http.request('GET','http://dev.markitondemand.com/Api/v2/Lookup/json?input=' + search_str)
+	strResponse = response.data.decode("utf-8")
+	listResponse = json.loads(strResponse)
+	fltrlistResponse = sorted([elem for elem in listResponse if elem['Exchange'] in ['NYSE', 'NASDAQ']],key = lambda f : f['Exchange'])
+	outputResponse = json.dumps(fltrlistResponse)
+	return outputResponse
+
+def main() :
+	print(getStockNames(sys.argv[1]))
+
+if __name__ == "__main__" : main()
