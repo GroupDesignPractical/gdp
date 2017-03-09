e73f88972bb134009283b7f1c493c95d0bf71429
parallelipopat <utsavp123@gmail.com>
2017-03-09 09:38:03 +0000

    Update getStockName.py

--- getStockName.py
+++ getStockName.py
@@ -8,10 +8,13 @@ http = urllib3.PoolManager()
 def getStockNames(input_str):
 	search_str = input_str.replace(' ','%20')
 	response = http.request('GET','http://dev.markitondemand.com/Api/v2/Lookup/json?input=' + search_str)
-	strResponse = response.data.decode("utf-8")
-	listResponse = json.loads(strResponse)
-	fltrlistResponse = sorted([elem for elem in listResponse if elem['Exchange'] in ['NYSE', 'NASDAQ']],key = lambda f : f['Exchange'])
-	outputResponse = json.dumps(fltrlistResponse)
+	if (response.status == 400) :
+		outputResponse = json.dumps("Error")
+	else:
+		strResponse = response.data.decode("utf-8")
+		listResponse = json.loads(strResponse)
+		fltrlistResponse = sorted([elem for elem in listResponse if elem['Exchange'] in ['NYSE', 'NASDAQ']],key = lambda f : f['Exchange'])
+		outputResponse = json.dumps(fltrlistResponse)
 	return outputResponse
 
 def main() :
