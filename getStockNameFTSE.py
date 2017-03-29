cfd43502afe7c3f276d6a3993ef5da9f21748c20
parallelipopat <utsavp123@gmail.com>
2017-03-29 23:26:56 +0530

    Update getStockNameFTSE.py

--- getStockNameFTSE.py
+++ getStockNameFTSE.py
@@ -6,13 +6,13 @@ import sys
 with open('FTSE350.json') as json_file:
     listFTSE350 = json.load(json_file)
 
-listCompanies = [i["Name"] for i in listFTSE350]
+listCompanies = [i['Name'] for i in listFTSE350]
 
 
 def getStockNameFTSE(input_str):
     listApproxMatches = process.extract(input_str, listCompanies)
     listResponse = [j for i in listApproxMatches for j in listFTSE350
-                    if j["Name"] == i[0]]
+                    if j['Name'] == i[0]]
     outputResponse = json.dumps(listResponse)
     return outputResponse
 
@@ -20,4 +20,4 @@ def getStockNameFTSE(input_str):
 def main():
     print(getStockNameFTSE(sys.argv[1]))
 
-if __name__ == "__main__": main()
+if __name__ == '__main__': main()
