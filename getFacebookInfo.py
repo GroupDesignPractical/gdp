8b7267c3ee1d8c425cf9673a6c7edcc443a3304f
AndreeaGanciulescu <andreeaganciulescu@gmail.com>
2017-04-15 11:35:42 +0300

    Search multiple result pages
    Program now receives an extra argument, specifying how far in the list
    of posts to go with the search.

--- getFacebookInfo.py
+++ getFacebookInfo.py
@@ -12,21 +12,30 @@ def initGraph():
   f = open('fbToken.txt', 'r')
   content = f.readlines()
   accessToken = content[0].strip()
-  return facebook.GraphAPI(access_token=accessToken, version='2.7')
+  return facebook.GraphAPI(access_token=accessToken)
 
 
-def getPostId(graph, newspage, link):
-  allPosts = graph.get_object(id=newspage, fields='posts', limit=100)
-  allPostIds = [p['id'] for p in allPosts['posts']['data']]
-  links = graph.get_objects(ids=allPostIds, fields='link')
-  # compare url hierarchic paths of given link and post link
-  postIds = [pId for pId in allPostIds
-             if getUrlPath(unshortenUrl(links[pId]['link']), 0) ==
-             getUrlPath(link, 1)]
-  print(postIds)
-  if postIds != []:
-    return postIds[0]
-  return None
+def getPostId(graph, newspage, path, maxCount):
+  allPosts = graph.get_object(id=newspage, fields='posts')['posts']
+  maxPageCount = maxCount / 25
+  if maxCount % 25 != 0:
+    maxPageCount += 1
+  pageCount = 0
+  postId = None
+  while postId is None and allPosts['data'] != [] and pageCount < maxPageCount:
+    pageCount += 1
+    allPostIds = [p['id'] for p in allPosts['data']]
+    links = graph.get_objects(ids=allPostIds, fields='link')
+    # compare url hierarchic paths of given link and post link
+    postIds = [pId for pId in allPostIds
+               if getUrlPath(unshortenUrl(links[pId]['link']), 0) ==
+               path]
+    print(postIds)
+    if postIds != []:
+      postId = postIds[0]
+    else:
+      allPosts = requests.get(allPosts['paging']['next']).json()
+  return postId
 
 
 def getUrlPath(url, flag):
@@ -40,9 +49,9 @@ def unshortenUrl(url):
   return requests.head(url, allow_redirects=True).url
 
 
-def getReactions(graph, newspage, link):
-    link = unshortenUrl(link)
-    postId = getPostId(graph, newspage, link)
+def getReactions(graph, newspage, link, maxCount):
+    path = getUrlPath(unshortenUrl(link), 1)
+    postId = getPostId(graph, newspage, path, maxCount)
     if postId is None:
       print("No facebook post about this article has been found.")
       return None
@@ -66,11 +75,17 @@ def getReactions(graph, newspage, link):
 
 #  argv[1] - Name of Facebook news page (e. g. bbcnews)
 #  argv[2] - Link to article
+#  argv[3] - (optional) number of posts to search through
+#            (defaults to 25, i.e. one page)
 def main():
   graph = initGraph()
   newspage = sys.argv[1]
   articleLink = sys.argv[2]
-  reactions = getReactions(graph, newspage, articleLink)
+  if len(sys.argv) > 3:
+    maxCount = int(sys.argv[3])
+  else:
+    maxCount = 25
+  reactions = getReactions(graph, newspage, articleLink, maxCount)
   if reactions is not None:
     print(reactions)
 
